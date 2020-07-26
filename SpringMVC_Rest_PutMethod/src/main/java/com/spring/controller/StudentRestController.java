22410d4a8a3d5a22c66965b0c0498b78fb89c590
trainig2020 <68064760+trainig2020@users.noreply.github.com>
2020-07-26 21:18:36 +0530

    Add files via upload

---
+++ SpringMVC_Rest_PutMethod/src/main/java/com/spring/controller/StudentRestController.java
@@ -0,0 +1,58 @@
+package com.spring.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.model.Student;
+
+@RestController
+public class StudentRestController {
+	
+	@RequestMapping(value = "/students", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
+	public List<Student> getStudentList(){
+		Student student1 = new Student();
+		student1.setStudentName("Ezhilarasi");
+		Student student2 = new Student();
+		student2.setStudentName("Sandhiya");
+		Student student3 = new Student();
+		student3.setStudentName("Ratheesh");
+		
+		List<Student> studentList  = new ArrayList<Student>();
+		studentList.add(student1);
+		studentList.add(student2);
+		studentList.add(student3);
+		return studentList;
+	}
+	
+	
+	@RequestMapping(value = "/students/{name}", method=RequestMethod.GET)
+	public Student getStudent(@PathVariable("name") String studentName){
+		Student student1 = new Student();
+		student1.setStudentName(studentName);
+		return student1;
+
+}
+	
+	@RequestMapping(value = "/students/{name}", method=RequestMethod.PUT, consumes=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Boolean> updateStudent(@PathVariable("name") String studentName, @RequestBody Student student){
+		System.out.println("Student Name :"+studentName);
+		System.out.println("StudentName : "+student.getStudentName()+" Age is "+ student.getAge());
+		
+		HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.add("key1", "value1");
+		httpHeaders.add("key2", "value2");
+		return new ResponseEntity<Boolean>(true,httpHeaders, HttpStatus.OK);
+
+}
+}
