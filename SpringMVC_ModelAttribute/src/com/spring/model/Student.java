78ecc775bcad969d09f083daae9ce4ce0ff975b3
trainig2020 <68064760+trainig2020@users.noreply.github.com>
2020-07-26 21:10:55 +0530

    Add files via upload

---
+++ SpringMVC_ModelAttribute/src/com/spring/model/Student.java
@@ -0,0 +1,60 @@
+package com.spring.model;
+
+
+
+import java.util.Date;
+import java.util.List;
+
+public class Student {
+	
+	private String studentName;
+	private int age;
+	
+	private long mobileNo;
+	private Date dob;
+	private List<String> skills;
+	
+	private Address address;
+	
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public long getMobileNo() {
+		return mobileNo;
+	}
+	public void setMobileNo(long mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+	public Date getDob() {
+		return dob;
+	}
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+	public List<String> getSkills() {
+		return skills;
+	}
+	public void setSkills(List<String> skills) {
+		this.skills = skills;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	
+	
+	
+	
+
+}
