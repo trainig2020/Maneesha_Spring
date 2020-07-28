ab68e19cbaedf2f45da07b789b4ebdbd6c2636ba
trainig2020 <68064760+trainig2020@users.noreply.github.com>
2020-07-28 23:21:26 +0530

    Add files via upload

---
+++ DepartmentCRUD_MVC/src/com/prathyusha/service/DepartmentService.java
@@ -0,0 +1,31 @@
+package com.prathyusha.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.prathyusha.dao.DepartmentDao;
+import com.prathyusha.dto.Department;
+
+@Service
+@Transactional
+public class DepartmentService {
+
+	@Autowired
+	private DepartmentDao departmentDao;
+	
+	public List<Department> getAllEmployees(){
+		System.out.println("In Service");
+		return departmentDao.getAllDepartments();
+	}
+	
+	public void insertDepartment(Department department) {
+		departmentDao.insertDepartment(department);
+	}
+	
+	public void deleteDepartment(int DeptID) {
+		departmentDao.deleteDepartment(DeptID);
+	}
+}
