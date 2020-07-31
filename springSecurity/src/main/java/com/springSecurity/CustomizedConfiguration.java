49866d162770fe61db845f16efd0d35888f12a59
trainig2020 <68064760+trainig2020@users.noreply.github.com>
2020-07-31 07:51:31 +0530

    Add files via upload

---
+++ springSecurity/src/main/java/com/springSecurity/CustomizedConfiguration.java
@@ -0,0 +1,39 @@
+package com.springSecurity;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class CustomizedConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		//We can set our own configuation
+		auth.inMemoryAuthentication().withUser("ezhil").password("ezhil").roles("USER")
+		.and()
+		.withUser("admin").password("ezhil").roles("ADMIN");
+	}
+	
+	@Bean
+	public PasswordEncoder getPassword(){
+		return NoOpPasswordEncoder.getInstance();
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers("/admin").hasRole("ADMIN")
+		//.antMatchers("/user").hasRole("USER")
+		.antMatchers("/user").hasAnyRole("USER","ADMIN")
+		.antMatchers("/").permitAll()
+		.and().formLogin();
+	}
+	
+	
+
+}
---
+++ springSecurity/src/main/java/com/springSecurity/controller/HomeController.java
@@ -0,0 +1,28 @@
+package com.springSecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	@GetMapping("/")
+	public String welcome(){
+		
+		return "Welcome";
+		
+	}
+	@GetMapping("/user")
+	public String user(){
+		
+		return "Welcome User";
+		
+	}
+	@GetMapping("/admin")
+	public String admin(){
+		
+		return "Welcome Admin";
+		
+	}
+
+}
